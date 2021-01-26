258e80cd800233771a6f2eb928ca725fc6553398
Zachinquarantine <69423184+Zachinquarantine@users.noreply.github.com>
2021-01-26 11:55:52 -0500

    Create cli.js

---
+++ cli.js
@@ -0,0 +1,9 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline.question(`Silverlight is dead in`, Silverlight is dead in => {
+  console.log(`${Silverlight is dead in} 259 days.`)
+  readline.close()
+})
